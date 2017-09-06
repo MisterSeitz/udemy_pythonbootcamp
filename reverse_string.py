a78e94efe998a0af287233aacbaa1d3f23acde1c
Muhammed Seitz <mseitz@ans.adac.ae>
2017-09-06 21:35:34 +0400

    Amend reverse_string.py:
    Allow continuous loop for user input instead of quit after 1st print.

--- reverse_string.py
+++ reverse_string.py
@@ -1,5 +1,10 @@
 # Reverse a String -
 # Enter a string and the program will reverse it and print it out.
 
-string = input('Enter a string to see the reverse result: ')
-print(string[::-1])
+
+x = 1
+while x == 1:
+    string = input('Enter a string to see the reverse result: ')
+    print(string[::-1])
+    continue
+
