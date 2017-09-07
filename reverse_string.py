edee78d34ca8be9efcce6275b8dfab8523de2732
Muhammed Seitz <mseitz@ans.adac.ae>
2017-09-07 07:01:43 +0400

    Amend 'reverse_string.py':
    Allow user to type Quit or quit to end program.

--- reverse_string.py
+++ reverse_string.py
@@ -1,10 +1,16 @@
 # Reverse a String -
 # Enter a string and the program will reverse it and print it out.
 
-
-x = 1
-while x == 1:
-    string = input('Enter a string to see the reverse result: ')
-    print(string[::-1])
-    continue
+while True:
+    string = input(
+        'Enter a string to see the reverse result: (Type Quit/quit to exit)\n')
+    if string == 'Quit':
+        print(string[::-1])
+        break
+    elif string =='quit':
+        print(string[::-1])
+        break
+    else:
+        print(string[::-1])
+        continue
 
