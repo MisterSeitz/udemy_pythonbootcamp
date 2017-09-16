9d77445dc071d55db89b5107d458db18f776c3cd
Muhammed Faiz Seitz <misterseitz999@gmail.com>
2017-09-16 16:48:19 +0400

    modified:   MilestoneProjects/tictactoe.py:
    Define functions for Win Determination
        p1condition()
        p2condition()
    
    Insert Comments to Explain Structure

--- MilestoneProjects/tictactoe.py
+++ MilestoneProjects/tictactoe.py
@@ -1,6 +1,7 @@
 nine = ['1', '2', '3', '4', '5' ,'6', '7', '8', '9']
 
-# Winning Lists:
+# Global Winning Lists:
+
 wl1 = ['1', '2', '3']
 wl2 = ['4', '5', '6']
 wl3 = ['7', '8', '9']
@@ -10,16 +11,21 @@ wl6 = ['3', '6', '9']
 wl7 = ['1', '5', '9']
 wl8 = ['3', '5', '7']
 
+# Global Default Board Lines
+
 line1 = '| 7 | 8 | 9 |'
 line2 = ' --- --- --- '
 line3 = '| 4 | 5 | 6 |'
 line4 = ' --- --- --- '
 line5 = '| 1 | 2 | 3 |'
 
+# Function That Prints Board Based On Current Global Line Values
 
 def printboard():
     global line1,line2,line3,line4,line5
     print('\n',line1,'\n',line2,'\n',line3,'\n',line4,'\n',line5,'\n')
+
+# Function That Resets Board To Global Default Lines
 
 def reset():    
     global line1,line2,line3,line4,line5
@@ -28,6 +34,8 @@ def reset():
     line3 = '| 4 | 5 | 6 |'
     line4 = ' --- --- --- '
     line5 = '| 1 | 2 | 3 |'
+
+# Funtion that replaces number with X for Player1
 
 def checkboardx():
     global line1,line2,line3,line4,line5
@@ -49,7 +57,9 @@ def checkboardx():
         line5 = line5.replace("2", "X")
     elif userinput in line5[10]:
         line5 = line5.replace("3", "X")
-        
+
+# Funtion that replaces number with O for Player2 
+
 def checkboardo():
     global line1,line2,line3,line4,line5
     if userinput in line1[2]:
@@ -71,6 +81,8 @@ def checkboardo():
     elif userinput in line5[10]:
         line5 = line5.replace("3", "O")
 
+# Player1 and Player2 Functions That Checks & Displays Board
+
 def player1():
     global userinput
     userinput = input('Player 1 (X), Please enter number (1-9): ')
@@ -91,9 +103,35 @@ def player2():
         checkboardo()
         printboard()
 
+# Win Determination Functions (Player1 and Player2)
+
+def p1condition():
+    global wl1,wl2,wl3,wl4,wl5,wl6,wl7,wl8
+    if all(item in p1 for item in wl1) == True or \
+    all(item in p1 for item in wl2) == True or \
+    all(item in p1 for item in wl3) == True or \
+    all(item in p1 for item in wl4) == True or \
+    all(item in p1 for item in wl5) == True or \
+    all(item in p1 for item in wl6) == True or \
+    all(item in p1 for item in wl7) == True or \
+    all(item in p1 for item in wl8) == True: 
+        return True
+
+def p2condition():
+    global wl1,wl2,wl3,wl4,wl5,wl6,wl7,wl8
+    if all(item in p2 for item in wl1) == True or \
+    all(item in p2 for item in wl2) == True or \
+    all(item in p2 for item in wl3) == True or \
+    all(item in p2 for item in wl4) == True or \
+    all(item in p2 for item in wl5) == True or \
+    all(item in p2 for item in wl6) == True or \
+    all(item in p2 for item in wl7) == True or \
+    all(item in p2 for item in wl8) == True: 
+         return True
+
+# Program Starts
 
 while True:
-    
     reset()
     p1 = []
     p2 = []
@@ -110,263 +148,50 @@ while True:
     player2()
     player1()
     player2()
+    
     player1()
-
-    if all(item in p1 for item in wl1) == True:
+    
+    if p1condition() == True:
         playagain = input('Player 1 Wins, Play Again (Y/N)? ')
         if playagain == 'n' or playagain =='N':
             break
         elif playagain == 'y' or playagain =='Y':
             continue
-    elif all(item in p1 for item in wl2) == True:
-        playagain = input('Player 1 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':         
-            continue
-    elif all(item in p1 for item in wl3) == True:
-        playagain = input('Player 1 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p1 for item in wl4) == True:
-        playagain = input('Player 1 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p1 for item in wl5) == True:
-        playagain = input('Player 1 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p1 for item in wl6) == True:
-        playagain = input('Player 1 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p1 for item in wl7) == True:
-        playagain = input('Player 1 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p1 for item in wl8) == True:
-        playagain = input('Player 1 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-        
+    
     player2()
 
-    if all(item in p2 for item in wl1) == True:
+    if p2condition() == True:
         playagain = input('Player 2 Wins, Play Again (Y/N)? ')
         if playagain == 'n' or playagain =='N':
             break
         elif playagain == 'y' or playagain =='Y':
-            continue
-    elif all(item in p2 for item in wl2) == True:
-        playagain = input('Player 2 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':          
-            continue
-    elif all(item in p2 for item in wl3) == True:
-        playagain = input('Player 2 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p2 for item in wl4) == True:
-        playagain = input('Player 2 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p2 for item in wl5) == True:
-        playagain = input('Player 2 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p2 for item in wl6) == True:
-        playagain = input('Player 2 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p2 for item in wl7) == True:
-        playagain = input('Player 2 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p2 for item in wl8) == True:
-        playagain = input('Player 2 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
             continue
         
     player1()
-
-    if all(item in p1 for item in wl1) == True:
+    
+    if p1condition() == True:
         playagain = input('Player 1 Wins, Play Again (Y/N)? ')
         if playagain == 'n' or playagain =='N':
             break
         elif playagain == 'y' or playagain =='Y':
-            continue
-    elif all(item in p1 for item in wl2) == True:
-        playagain = input('Player 1 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            p1 = []
-            p2 = []          
-            continue
-    elif all(item in p1 for item in wl3) == True:
-        playagain = input('Player 1 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            p1 = []
-            p2 = []
-            continue
-    elif all(item in p1 for item in wl4) == True:
-        playagain = input('Player 1 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p1 for item in wl5) == True:
-        playagain = input('Player 1 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p1 for item in wl6) == True:
-        playagain = input('Player 1 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p1 for item in wl7) == True:
-        playagain = input('Player 1 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p1 for item in wl8) == True:
-        playagain = input('Player 1 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
             continue
 
     player2()
 
-    if all(item in p2 for item in wl1) == True:
+    if p2condition() == True:
         playagain = input('Player 2 Wins, Play Again (Y/N)? ')
         if playagain == 'n' or playagain =='N':
             break
         elif playagain == 'y' or playagain =='Y':
-            continue
-    elif all(item in p2 for item in wl2) == True:
-        playagain = input('Player 2 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':          
-            continue
-    elif all(item in p2 for item in wl3) == True:
-        playagain = input('Player 2 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p2 for item in wl4) == True:
-        playagain = input('Player 2 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p2 for item in wl5) == True:
-        playagain = input('Player 2 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p2 for item in wl6) == True:
-        playagain = input('Player 2 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p2 for item in wl7) == True:
-        playagain = input('Player 2 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p2 for item in wl8) == True:
-        playagain = input('Player 2 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
             continue
 
     player1()
-
-    if all(item in p1 for item in wl1) == True:
+    
+    if p1condition() == True:
         playagain = input('Player 1 Wins, Play Again (Y/N)? ')
         if playagain == 'n' or playagain =='N':
             break
         elif playagain == 'y' or playagain =='Y':
-            continue
-    elif all(item in p1 for item in wl2) == True:
-        playagain = input('Player 1 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':          
-            continue
-    elif all(item in p1 for item in wl3) == True:
-        playagain = input('Player 1 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p1 for item in wl4) == True:
-        playagain = input('Player 1 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p1 for item in wl5) == True:
-        playagain = input('Player 1 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p1 for item in wl6) == True:
-        playagain = input('Player 1 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p1 for item in wl7) == True:
-        playagain = input('Player 1 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
-            continue
-    elif all(item in p1 for item in wl8) == True:
-        playagain = input('Player 1 Wins, Play Again (Y/N)? ')
-        if playagain == 'n' or playagain == 'N':
-            break
-        elif playagain == 'y' or playagain == 'Y':
             continue
     else:
         playagain = input('Draw Game!, Play Again (Y/N)? ')
